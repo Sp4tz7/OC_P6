0e4e6b34535e41cea4c6876f8001d16ca96b8610
David <ds@magneticlab.ch>
2020-08-22 13:43:37 +0200

    ADD: Register controller
    - Added mailer library
    - Added JWT library

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,8 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Form\UserProfileType;
+use App\Form\UserRegisterType;
+use App\Repository\UserRepository;
 use App\Security\LoginFormAuthenticator;
+use Lcobucci\JWT\Builder;
+use Lcobucci\JWT\Signer\Hmac\Sha256;
+use Lcobucci\JWT\Signer\Key;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
@@ -11,20 +18,19 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
-use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * Class UserController
- * @package App\Controller
+ * Class UserController.
  */
 class UserController extends AbstractController
 {
     /**
      * @Route("/login", name="app_login")
-     * @param AuthenticationUtils $authenticationUtils
-     * @return Response
      */
     public function loginUser(AuthenticationUtils $authenticationUtils): Response
     {
@@ -45,8 +51,6 @@ class UserController extends AbstractController
 
     /**
      * @Route("/logout", name="app_logout")
-     * @param AuthenticationUtils $authenticationUtils
-     * @return Response
      */
     public function logoutUser(AuthenticationUtils $authenticationUtils): Response
     {
@@ -60,9 +64,6 @@ class UserController extends AbstractController
 
     /**
      * @Route("/profile", name="profile", methods={"GET", "POST"})
-     * @param Request    $request
-     * @param Filesystem $filesystem
-     * @return Response
      */
     public function userProfile(Request $request, Filesystem $filesystem): Response
     {
@@ -104,14 +105,9 @@ class UserController extends AbstractController
 
     /**
      * @Route("/profile/password", name="profile-password", methods={"GET", "POST"})
-     * @param Request                      $request
-     * @param LoginFormAuthenticator       $login
-     * @param UserPasswordEncoderInterface $encoder
-     * @return Response
      */
     public function userUpdatePassword(
         Request $request,
-        LoginFormAuthenticator $login,
         UserPasswordEncoderInterface $encoder
     ): Response {
         $form = $this->createFormBuilder()
@@ -123,29 +119,113 @@ class UserController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted()) {
-            if ($login::comparePasswords(
-                $form['password']->getData(),
-                $form['rePassword']->getData()
-            )) {
-                $entityManager = $this->getDoctrine()->getManager();
-                $user = $this->getUser();
-                $user->setPassword($encoder->encodePassword($user, $form['password']->getData()));
-                $entityManager->persist($user);
-                $entityManager->flush();
+            $this->validatePassword($form['password']->getData(), $form['rePassword']->getData());
 
-                $this->addFlash('success', 'Password has been changed');
+            $entityManager = $this->getDoctrine()->getManager();
+            $user = $this->getUser();
+            $user->setPassword($encoder->encodePassword($user, $form['password']->getData()));
+            $entityManager->persist($user);
+            $entityManager->flush();
 
-                return $this->redirectToRoute('profile');
+            $this->addFlash('success', 'Password has been changed');
 
-            } else {
-                $this->addFlash('error', 'The passwords does not matches');
-            }
+            return $this->redirectToRoute('profile');
         }
 
         return $this->render(
             'user/password.html.twig',
             [
                 'form' => $form->createView(),
+            ]
+        );
+    }
+
+    protected function validatePassword($password, $rePassword)
+    {
+        if (!LoginFormAuthenticator::comparePasswords($password, $rePassword)) {
+            $this->addFlash('error', 'The passwords does not matches');
+
+            return $this->redirectToRoute('profile-password');
+        }
+    }
+
+    /**
+     * @Route("/account/activate/{token}", name="activate-account", methods={"GET"})
+     */
+    public function userActivateAccount(UserRepository $userRepository, $token)
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $user = $userRepository->findOneBy(['token' => $token]);
+        if ($user) {
+            $user->setIsActive(true);
+            $user->setToken('');
+            $entityManager->flush($user);
+            $this->addFlash('success', 'Your account has been activated. Please Login!');
+
+            return $this->redirectToRoute('app_login');
+        }
+
+        throw $this->createNotFoundException('The token is not valid');
+    }
+
+    /**
+     * @Route("/register", name="register", methods={"GET", "POST"})
+     */
+    public function userRegister(
+        Request $request,
+        UserPasswordEncoderInterface $encoder,
+        MailerInterface $mailer
+    ): Response {
+        if ($this->getUser()) {
+            return $this->redirectToRoute('profile');
+        }
+
+        $user = new User();
+        $form = $this->createForm(UserRegisterType::class, $user);
+        $form->handleRequest($request);
+        $error = [];
+
+        if ($form->isSubmitted()) {
+            $this->validatePassword($form['password']->getData(), $form['rePassword']->getData());
+
+            $time = time();
+            $signer = new Sha256();
+            $token = (new Builder())
+                ->withClaim('mail', $user->getEmail())
+                ->expiresAt($time + 600)
+                ->getToken($signer, new Key($_ENV['APP_SECRET']));
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $user->setToken($token);
+            $user->setIsActive();
+            $user->setRoles(['ROLE_USER']);
+            $user->setPassword($encoder->encodePassword($user, $form['password']->getData()));
+            $entityManager->persist($user);
+            $entityManager->flush();
+            $email = (new TemplatedEmail())
+                ->from($_ENV['MAILER_FROM'])
+                ->to(new Address($user->getEmail(), $user->getFirstname()))
+                ->bcc($_ENV['MAILER_FROM'])
+                ->replyTo($_ENV['MAILER_REPLY_TO'])
+                ->subject('Your new account')
+                ->text('Congratulation, your account has been created.')
+                ->htmlTemplate('emails/registration.html.twig')
+                ->context(
+                    [
+                        'user' => $user,
+                    ]
+                );
+
+            $mailer->send($email);
+
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render(
+            'user/register.html.twig',
+            [
+                'form' => $form->createView(),
+                'error' => $error,
             ]
         );
     }
