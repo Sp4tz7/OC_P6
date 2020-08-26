c337bbbc371d5469ddd1a759cd935a3a208c555a
Sp4tz7 <sp4tz7@gmail.com>
2020-08-26 09:09:25 +0200

    ADD: Comment entity
    - Added comment entity
    - Added comments relation in User
    - Added comments relation in Trick
    - Added fixtures

--- src/DataFixtures/TrickFixtures.php
+++ src/DataFixtures/TrickFixtures.php
@@ -9,21 +9,12 @@ use Doctrine\Common\Persistence\ObjectManager;
 
 class TrickFixtures extends Fixture implements DependentFixtureInterface
 {
+    public const MUTE_TRICK_REFERENCE = 'trick-mute';
+
     public function load(ObjectManager $manager)
     {
         $trick = new Trick();
-        $date = new \DateTime('2020-01-01');
-        $trick->setName('Mute');
-        $trick->setSlug('mute');
-        $trick->setDescription('Saisie de la carre frontside de la planche entre les deux pieds avec la main avant');
-        $trick->setAddedBy($this->getReference(UserFixtures::ADMIN_USER_REFERENCE));
-        $trick->addCategory($this->getReference(TrickCategoryFixtures::GRAB_CATEGORY_REFERENCE));
-        $trick->setDateAdd($date);
-        $trick->setImage('default_image.jpeg');
-        $manager->persist($trick);
-
-        $trick = new Trick();
-        $date = new \DateTime('2020-02-01');
+        $date = new \DateTime();
         $trick->setName('Sad');
         $trick->setSlug('sad');
         $trick->setDescription('Saisie de la carre frontside de la planche, entre les deux pieds, avec la main arrière');
@@ -34,7 +25,7 @@ class TrickFixtures extends Fixture implements DependentFixtureInterface
         $manager->persist($trick);
 
         $trick = new Trick();
-        $date = new \DateTime('2020-03-01');
+        $date = new \DateTime();
         $trick->setName('Indy ');
         $trick->setSlug('indy');
         $trick->setDescription('Saisie de la carre backside de la planche entre les deux pieds avec la main arrière ');
@@ -45,7 +36,7 @@ class TrickFixtures extends Fixture implements DependentFixtureInterface
         $manager->persist($trick);
 
         $trick = new Trick();
-        $date = new \DateTime('2020-03-01');
+        $date = new \DateTime();
         $trick->setName('Stalefish');
         $trick->setSlug('stalefish');
         $trick->setDescription('Saisie de la carre backside de la planche entre les deux pieds avec la main arrière ');
@@ -56,7 +47,7 @@ class TrickFixtures extends Fixture implements DependentFixtureInterface
         $manager->persist($trick);
 
         $trick = new Trick();
-        $date = new \DateTime('2020-03-01');
+        $date = new \DateTime();
         $trick->setName('Tail grab');
         $trick->setSlug('tail-grab');
         $trick->setDescription('Saisie de la partie arrière de la planche, avec la main arrière');
@@ -67,7 +58,7 @@ class TrickFixtures extends Fixture implements DependentFixtureInterface
         $manager->persist($trick);
 
         $trick = new Trick();
-        $date = new \DateTime('2020-03-01');
+        $date = new \DateTime();
         $trick->setName('Nose grab');
         $trick->setSlug('nose-grab');
         $trick->setDescription('Saisie de la partie avant de la planche, avec la main avant');
@@ -78,7 +69,7 @@ class TrickFixtures extends Fixture implements DependentFixtureInterface
         $manager->persist($trick);
 
         $trick = new Trick();
-        $date = new \DateTime('2020-03-01');
+        $date = new \DateTime();
         $trick->setName('Japan air');
         $trick->setSlug('japan-air');
         $trick->setDescription('Saisie de l\'avant de la planche, avec la main avant, du côté de la carre frontside');
@@ -89,7 +80,7 @@ class TrickFixtures extends Fixture implements DependentFixtureInterface
         $manager->persist($trick);
 
         $trick = new Trick();
-        $date = new \DateTime('2020-03-01');
+        $date = new \DateTime();
         $trick->setName('Seat belt');
         $trick->setSlug('seat-belt');
         $trick->setDescription('Saisie du carre frontside à l\'arrière avec la main avante');
@@ -100,7 +91,7 @@ class TrickFixtures extends Fixture implements DependentFixtureInterface
         $manager->persist($trick);
 
         $trick = new Trick();
-        $date = new \DateTime('2020-03-01');
+        $date = new \DateTime();
         $trick->setName('Truck driver');
         $trick->setSlug('truck-driver');
         $trick->setDescription('Saisie du carre avant et carre arrière avec chaque main (comme tenir un volant de voiture');
@@ -111,7 +102,7 @@ class TrickFixtures extends Fixture implements DependentFixtureInterface
         $manager->persist($trick);
 
         $trick = new Trick();
-        $date = new \DateTime('2020-03-01');
+        $date = new \DateTime();
         $trick->setName('180');
         $trick->setSlug('180');
         $trick->setDescription('Un 180 désigne un demi-tour, soit 180 degrés d\'angle');
@@ -121,7 +112,20 @@ class TrickFixtures extends Fixture implements DependentFixtureInterface
         $trick->setImage('default_image.jpeg');
         $manager->persist($trick);
 
+        $trick = new Trick();
+        $date = new \DateTime();
+        $trick->setName('Mute');
+        $trick->setSlug('mute');
+        $trick->setDescription('Saisie de la carre frontside de la planche entre les deux pieds avec la main avant');
+        $trick->setAddedBy($this->getReference(UserFixtures::ADMIN_USER_REFERENCE));
+        $trick->addCategory($this->getReference(TrickCategoryFixtures::GRAB_CATEGORY_REFERENCE));
+        $trick->setDateAdd($date);
+        $trick->setImage('default_image.jpeg');
+        $manager->persist($trick);
+
         $manager->flush();
+
+        $this->addReference(self::MUTE_TRICK_REFERENCE, $trick);
     }
 
     public function getDependencies()
--- src/Entity/Trick.php
+++ src/Entity/Trick.php
@@ -76,11 +76,17 @@ class Trick
      */
     private $videos;
 
+    /**
+     * @ORM\OneToMany(targetEntity=Comment::class, mappedBy="trick", orphanRemoval=true)
+     */
+    private $comments;
+
     public function __construct()
     {
         $this->category = new ArrayCollection();
         $this->images = new ArrayCollection();
         $this->videos = new ArrayCollection();
+        $this->comments = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -243,6 +249,37 @@ class Trick
             // set the owning side to null (unless already changed)
             if ($video->getTrick() === $this) {
                 $video->setTrick(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Comment[]
+     */
+    public function getComments(): Collection
+    {
+        return $this->comments;
+    }
+
+    public function addComment(Comment $comment): self
+    {
+        if (!$this->comments->contains($comment)) {
+            $this->comments[] = $comment;
+            $comment->setTrick($this);
+        }
+
+        return $this;
+    }
+
+    public function removeComment(Comment $comment): self
+    {
+        if ($this->comments->contains($comment)) {
+            $this->comments->removeElement($comment);
+            // set the owning side to null (unless already changed)
+            if ($comment->getTrick() === $this) {
+                $comment->setTrick(null);
             }
         }
 
