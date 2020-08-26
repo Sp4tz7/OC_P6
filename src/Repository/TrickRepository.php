f4ee67b98f69522e0fd476f3553bde1fe33d8229
Sp4tz7 <sp4tz7@gmail.com>
2020-08-26 08:24:00 +0200

    ADD: delete trick controller

--- src/Repository/TrickRepository.php
+++ src/Repository/TrickRepository.php
@@ -19,6 +19,7 @@ class TrickRepository extends ServiceEntityRepository
         parent::__construct($registry, Trick::class);
     }
 
+
     // /**
     //  * @return Trick[] Returns an array of Trick objects
     //  */
