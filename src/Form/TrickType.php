b0f346e4e7ce9fc3a4c43e54ab6f5dd06fcd8d5b
Sp4tz7 <sp4tz7@gmail.com>
2020-09-10 15:26:03 +0200

    ADD: Edit & delete function

--- src/Form/TrickType.php
+++ src/Form/TrickType.php
@@ -8,13 +8,12 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\File;
 
-class TrickNewType extends AbstractType
+class TrickType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
@@ -69,7 +68,19 @@ class TrickNewType extends AbstractType
                     'class' => TrickCategory::class,
                     'multiple' => true,
                 ]
-            );
+            )
+            ->add('comments', CollectionType::class, [
+                'entry_type' => CommentType::class,
+                'required' => false,
+                'entry_options' => [
+                    'label' => 'Comment',
+                ],
+                'label' => false,
+                'by_reference' => false,
+                'allow_add' => true,
+                'allow_delete' => true,
+                'prototype' => true,
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
